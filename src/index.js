8e35ab95d05af04ce34158068ad0ad3bfc9d0f0e
Suzanne Knop <sknop8@gmail.com>
2018-10-11 18:39:05 -0400

    Operator component.

--- src/index.js
+++ src/index.js
@@ -2,33 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-class App extends Component {
+class SHOP2 extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      tasks: []
+      tasks: props.taskList
     }
-    // this.createTasks()
-  }
-
-  componentDidMount() {
-    this.createTasks()
-  }
-
-  createTasks() {
-    const taskList = [
-      {
-        taskSymbol:"go",
-        isPrimitive:false,
-        arguments:["place1", "place2"]
-      },
-      {
-        taskSymbol:"walk",
-        isPrimitive:true,
-        arguments:["place1", "place2"]
-      }
-    ]
-    this.setState({tasks: taskList})
   }
 
   getTasks() {
@@ -36,7 +15,7 @@ class App extends Component {
       (t) => { 
         return (
           <Task 
-            taskSymbol={ t.taskSymbol }
+            name={ t.name }
             isPrimitive={ t.isPrimitive }
             arguments={ t.arguments } /> 
         )
@@ -46,6 +25,7 @@ class App extends Component {
 
 
   render() {
+    var sampleTask = <Task name={"sample"} isPrimitive={true} />
     return (
       <div>
         <div>
@@ -55,28 +35,49 @@ class App extends Component {
         <p>Tasks: </p>
         { this.getTasks() }
 
-        {/* <Operator */}
-          {/* /> */}
+        <Operator task={ sampleTask } />
+        
       </div>
     )
   }
 }
 
+SHOP2.propTypes = {
+  taskList: PropTypes.array.isRequired
+}
+
+
+
+
 // TODO: Move major components into separate files
+
+/**
+ * Domain: Set of methods, operators, and axioms defining the domain.
+ */
+class Domain extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
 
 
 /**
  * Task: An activity to perform.
  */
 class Task extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
+  constructor(props) {
+    super(props)
+  }
 
   render() {
     return (
       <div>
-        <div>taskSymbol: { this.props.taskSymbol }</div>
+        <div>Task</div>
+        <div>name: { this.props.name }</div>
         <div>isPrimitive: { this.props.isPrimitive.toString() }</div>
         <div>arguments: { this.props.arguments.toString() }</div>
         <br />
@@ -84,33 +85,80 @@ class Task extends Component {
     )
   }
 }
-
 Task.propTypes = {
-  taskSymbol: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   isPrimitive: PropTypes.bool.isRequired,
   arguments: PropTypes.array
 }
+Task.defaultProps = {
+  arguments: []
+}
+
+
 
 /**
  * Operator: How a primitive task can be performed.
  */
 class Operator extends Component {
+  constructor(props) {
+    super(props)
+    // console.log(this.props.task.props.name)
+  }
+  
   render() {
     return (
       <div>
-
+        <div>Operator</div>
+        <div>Associated task: { this.props.task.props.name }</div>
+        <div>preconditions: {this.props.preconditions}</div>
+        <div>deleteList: {}</div>
+        <div>addList: {}</div>
       </div>
     )
   }
 }
-
 Operator.propTypes = {
-  taskSymbol: PropTypes.string.isRequired,
+  task: PropTypes.element.isRequired,
   preconditions: PropTypes.array,
   deleteList: PropTypes.array,
   addList: PropTypes.array
 }
+Operator.defaultProps = {
+  preconditions: [],
+  deleteList: [],
+  addList: []
+}
+
+/**
+ * Method: Specifies how to decompose a compount task into a partially ordered set of subtasks
+ */
+class Method extends Component {
+
+}
+
+/**
+ * Axiom: Used to specify preconditions ot methods and operators
+ */
+class Axiom extends Component {
+
+}
+
+
+
+
+const taskList = [
+  {
+    name: "go",
+    isPrimitive: false,
+    arguments: ["place1", "place2"]
+  },
+  {
+    name: "walk",
+    isPrimitive: true,
+    arguments: ["place1", "place2"]
+  }
+]
 
 ReactDOM.render(
-  <App />,
+  <SHOP2 taskList={ taskList } />,
   document.getElementById('root'));
