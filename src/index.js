7ff0e3ed176b5569bd812c2d6613178ed4fcce7f
Suzanne Knop <sknop8@gmail.com>
2018-11-08 21:27:45 -0500

    Axioms and precondition checking.

--- src/index.js
+++ src/index.js
@@ -6,72 +6,136 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      allTasks: props.taskList,
-      validTasks: [],
+      allTasks: props.taskList.slice(0), // Copy
+      tasks: props.taskList,
       operators: props.operatorList,
-      storyState: [
-         "Philadelphia"
-      ], // TODO: Find out how to do this
-      plan: []
+      storyState: props.storyState,
+      plan: [],
+      debugMode: true
     }
+
+    this.SHOP2 = this.SHOP2.bind(this)
+    this.processTasks = this.processTasks.bind(this)
+    this.reset = this.reset.bind(this)
   }
 
   findOperatorWithTask(task) {
     return this.state.operators.find( o => o.props.taskName == task.props.name)
   }
 
-  SHOP2() {
-    // Initialize validTasks with tasks that have no preconditions.
-    for (const t of this.state.allTasks) {
+  preconditionMet(p) {
+    const state = this.state.storyState.find( s => s.props.name == p.props.name)
+    return state && state.props.value == p.props.value
+  }
+
+  findValidTasks(taskList) {
+    let result = []
+    for (const t of taskList) {
       if (t.props.isPrimitive) {
         let op = this.findOperatorWithTask(t)
         if (op && op.props.preconditions.length == 0)
-          this.state.validTasks.push(t)
+          result.push(t)
+        else if (this.state.plan.length > 0) {
+          // Check state to see if preconditions are met
+          let pass = true
+          for (const p of op.props.preconditions) {
+            pass &= this.preconditionMet(p)
+          }
+          if (pass) result.push(t)
+        }
       } else {
         // TODO: Find the corresponding method and check for preconditions
       }
     }
+    return result
+  }
 
+  processTasks(validTasks) {      
     // Nondeterministically choose a task in validTasks
-    let rand = Math.floor(Math.random() * this.state.validTasks.length)
-    let task = this.state.validTasks[rand]
+    let rand = Math.floor(Math.random() * validTasks.length)
+    let task = validTasks[rand]
 
+    if(!task) return;
     console.log("chosen task: " + task.props.name)
 
     if (task.props.isPrimitive) {
       let op = this.findOperatorWithTask(task)
-      console.log(op)
-      // Apply the operator to the state by applying the addList and deleteList
-      // (TODO)
+      // TODO: Apply the operator to the state by applying the addList and deleteList
 
-      this.state.plan.push(op)
+      this.setState({plan: [...this.state.plan, op]})
+      this.setState({
+        tasks: this.state.tasks.filter( (_, i) => i !== this.state.tasks.indexOf(task)) 
+      })
+    } else {
+      // TODO: Find the method to decompose task into subtasks
+      // TODO: Set validTasks to all tasks in the subtasks with no preconditions
 
     }
     
   }
 
+  SHOP2() {
+    // Initialize validTasks with tasks that have no preconditions.
+    
+    if (this.state.tasks.length > 0) {
+      this.processTasks(this.findValidTasks(this.state.tasks) )
+    }
+  }
+
+  reset() { 
+    this.setState({
+      tasks: this.state.allTasks,
+      plan: []
+    })
+  }
 
   render() {
-    const primitiveTask = <Task name={"primitive-task"} isPrimitive={true} />
-    const compoundTask = <Task name={"compound-task"} isPrimitive={false} />
-    this.SHOP2()
+    // const primitiveTask = <Task name={"primitive-task"} isPrimitive={true} />
+    // const compoundTask = <Task name={"compound-task"} isPrimitive={false} />
+
+    if (!this.state.debugMode)
+      this.SHOP2() // Calculates the whole plan at once
+
 
     return (
       <div>
         <div>
           SHOP2 HTN algorithm implementation
         </div>
-        <h3>Tasks: </h3>
-        { this.state.allTasks }
+ 
 
         <br />
+        <button onClick={ this.SHOP2 }> go! </button>
+        <button onClick={ this.reset }>reset</button>
+
+        <div>
+        { this.state.debugMode && 
+          <div>
+            <h3>state (debug mode)</h3>
+            tasks: { this.state.validTasks } <br/>
+            valid tasks: { this.state.validTasks }<br/>
+            all tasks: { this.state.allTasks }
+          </div>
+        }
+        </div>
+        <br />
+      
 
         <h3>The Plan: </h3>
         { this.state.plan }
         <br />
-      
-        {/* <p>All Operators: </p>
-        { this.state.operators } */}
+
+        <h3>Story State:</h3>
+        { this.state.storyState.map((s) => {
+          return (<div>{s}</div>)
+        })}
+
+
+        <h3>All Tasks: </h3>
+        { this.state.allTasks }
+
+        <h3>All Operators: </h3>
+        { this.state.operators }
 
         {/* <Operator task={ primitiveTask } /> */}
 
@@ -141,6 +205,9 @@ Task.defaultProps = {
  * Operator: How a primitive task can be performed.
  */
 class Operator extends Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       <div>
@@ -167,7 +234,7 @@ Operator.defaultProps = {
 }
 
 /**
- * Method: Specifies how to decompose a compount task into a partially ordered set of subtasks
+ * Method: Specifies how to decompose a compound task into a partially ordered set of subtasks
  */
 class Method extends Component {
   render() {
@@ -191,10 +258,25 @@ Method.defaultProps = {
 }
 
 /**
- * Axiom: Used to specify preconditions ot methods and operators
+ * Axiom: Used to specify preconditions to methods and operators
  */
 class Axiom extends Component {
-  // TODO
+  render() {
+    return (
+      <div>
+        <span>Axiom( </span>
+        <span>name: {this.props.name}, </span>
+        <span>operator: {this.props.operator}, </span>
+        <span>value: {this.props.value} ) </span>
+      </div>
+    )
+  }
+
+  // attributes to compare in the state --> e.g. { isHuman: true }
+  // attrName: "isHuman", "location", "money"
+  // operator: "==", ">", or "<" for now
+  // value: "true", "philadelphia", "100", etc
+
 }
 
 
@@ -202,30 +284,66 @@ class Axiom extends Component {
 const taskList = [
   <Task name={ "go" }
     isPrimitive={ false } 
-    arguments={ ["place1", "place2"] }/>,
+    arguments={ ["place1", "place2"] }
+    key="go"
+    />,
   <Task name={ "walk" }
     isPrimitive={ true } 
-    arguments={ ["place1", "place2"] }/>,
+    arguments={ ["place1", "place2"] }
+    key="walk"
+    />,
   <Task name={ "run" }
     isPrimitive={ true } 
-    arguments={ ["place1", "place2"] }/>,
+    arguments={ ["place1", "place2"] }
+    key="run"
+    />,
   <Task name={ "fly" }
     isPrimitive={ true } 
-    arguments={ ["place1", "place2"] }/>
+    arguments={ ["place1", "place2"] }
+    key="fly"
+    />,
+  <Task name={ "gallop" }
+    isPrimitive={ true } 
+    arguments={ ["place1", "place2"] }
+    key="gallop"
+    />
 ]
+
+const isBird = <Axiom name="animal" operator="==" value="bird" key="animalbird"/>
+const isHuman = <Axiom name="animal" operator="==" value="human" key="animalhuman"/>
+const isHorse = <Axiom name="animal" operator="==" value="horse" key="animalhuman"/>
 
 const operatorList = [
   <Operator taskName={ "walk" } 
+    preconditions={ [isHuman] }
     addlist={ ["place2"] }
-    deleteList={ ["place1" ]} />,
+    deleteList={ ["place1" ]} 
+    key={ "walk" }
+    />,
   <Operator taskName={ "run" }
     addlist={ ["place2"] }
-    deleteList={ ["place1" ]} />,
-  <Operator taskName={ "fly" } preconditions={ ["is-bird"] }
+    deleteList={ ["place1" ]} 
+    key={ "run" }
+    />,
+  <Operator taskName={ "fly" } 
+    preconditions={ [isBird] }
     addlist={ ["place2"] }
-    deleteList={ ["place1" ]} />
+    deleteList={ ["place1" ]} 
+    key={ "fly" }
+    />,
+  <Operator taskName={ "gallop" }
+    preconditions={ [isHorse] }
+    addlist={ ["place2"] }
+    deleteList={ ["place1" ]} 
+    key={ "gallop" }
+    />
+]
+
+const storyState = [
+  <Axiom name={ "animal" } operator={ "==" } value={ "human" } />,
+  <Axiom name={ "location" } operator={ "==" } value={ "philadelphia" } />,
 ]
 
 ReactDOM.render(
-  <App taskList={ taskList } operatorList={ operatorList }/>,
+  <App taskList={ taskList } operatorList={ operatorList } storyState={ storyState } />,
   document.getElementById('root'));
